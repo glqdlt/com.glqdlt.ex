1f69d80779e05c2c3cec77f6d058190fcaca99a5
jhun <jhun@nm-4ones.com>
2019-01-24 13:39:25 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/nodaomybatis/NoDaoMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.glqdlt.ex.nodaomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoDaoMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoDaoMybatisApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/glqdlt/ex/nodaomybatis/NoDaoMybatisApplicationTests.java
@@ -0,0 +1,17 @@
+package com.glqdlt.ex.nodaomybatis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoDaoMybatisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
